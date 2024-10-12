58125305a5093483d164e507fa5baa6249c3472e
Etana87 <etana87@gmail.com>
2024-10-12 19:06:44 +0200

    Ejercicio

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,30 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+// VARIABLES
+let turno: number = 0;
+
+// ELEMENTOS
+const numberDisplay = document.getElementById("number") as HTMLButtonElement;
+const prevButton = document.getElementById("prevButton") as HTMLButtonElement;
+const nextButton = document.getElementById("nextButton") as HTMLButtonElement;
+
+//FUNCIÓN ACTUALIZAR A DOS NÚMEROS
+function actualizarDisplay() {
+     numberDisplay.innerText = turno.toString().padStart(2, "0");
+   }
+
+//CAMBIAR DE TURNO
+// Botón "Siguiente"
+nextButton.addEventListener("click", () => {
+    turno++;
+    actualizarDisplay();
+});
+  
+// Botón "Anterior"
+prevButton.addEventListener("click", () => {
+    if (turno > 0) {
+        turno--;
+        actualizarDisplay();
+    }
+});
+
