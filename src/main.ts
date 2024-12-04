0442a42c774b860db25474fec59088f8a7fcf88a
Etana87 <etana87@gmail.com>
2024-12-04 10:49:26 +0100

    Cambios

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,30 @@ let turno: number = 0;
 const numberDisplay = document.getElementById("number");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
+const turnoInput = document.getElementById("turnoInput");
+const setTurnoButton = document.getElementById("setTurnoButton");
 
-//FUNCIÓN ACTUALIZAR A DOS NÚMEROS
+// FUNCIÓN ACTUALIZAR A DOS NÚMEROS
 function actualizarDisplay() {
-    if (numberDisplay && numberDisplay instanceof HTMLButtonElement) {
+    if (numberDisplay && numberDisplay instanceof HTMLHeadingElement) {
         numberDisplay.innerText = turno.toString().padStart(2, "0");
     } else {
         console.error("actualizarDisplay: No se ha encontrado el elemento con id number");
-        } 
-   }
+    }
+}
+actualizarDisplay();
 
-//CAMBIAR DE TURNO
+// CAMBIAR DE TURNO
 // Botón "Siguiente"
 nextButton?.addEventListener("click", () => {
-    if(nextButton && nextButton instanceof HTMLHeadingElement) {
+    if (nextButton && nextButton instanceof HTMLButtonElement) {
         turno++;
         actualizarDisplay();
     } else {
         console.error("nextButton: No se ha encontrado el elemento con id nextButton");
     }
 });
-  
+
 // Botón "Anterior"
 prevButton?.addEventListener("click", () => {
     if (prevButton && prevButton instanceof HTMLButtonElement) {
@@ -40,3 +43,15 @@ prevButton?.addEventListener("click", () => {
     }
 });
 
+// Cambiar turno
+setTurnoButton?.addEventListener("click", () => {
+    if (turnoInput && turnoInput instanceof HTMLInputElement) {
+        const nuevoTurno = parseInt(turnoInput.value, 10);
+        if (!isNaN(nuevoTurno) && nuevoTurno >= 0) {
+            turno = nuevoTurno;
+            actualizarDisplay();
+        } else {
+            console.error("setTurnoButton: no se ha encontrado el elemento id setTurnoButton.");
+        }
+    }
+});
